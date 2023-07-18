66cac4cb5e8c7d8922b6e04cd5f8bd1ad940ee76
akramBNA <akrambenaoun1993@gmail.com>
2023-07-19 00:52:00 +0100

    main commit

---
+++ src/main/java/com/mycompany/calculator/Calculator.java
@@ -0,0 +1,20 @@
+package com.mycompany.calculator;
+
+
+/**
+ *
+ * @author akram
+ */
+public class Calculator {
+
+    public static void main(String[] args) {
+        CalculatorView theView = new CalculatorView();
+        
+    	CalculatorModel theModel = new CalculatorModel();
+        
+        CalculatorController theController = new CalculatorController(theView,theModel);
+        
+        theView.setVisible(true);
+        
+    }
+}
